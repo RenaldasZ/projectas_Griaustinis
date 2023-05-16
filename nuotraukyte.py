1f9e7c08a9702c750757e1fc46bb506d47497449
EricWebDev <erikasj1999@gmail.com>
2023-05-16 15:21:43 +0300

    la

---
+++ nuotraukyte.py
@@ -0,0 +1,9 @@
+
+from PIL import Image
+
+
+image = Image.open("warrior.png")
+width, height = (564, 846)
+new_size = (150, 200) # vietoje "width" ir "height" įrašykite norimus parametrus, pvz.: (60, 80)
+resized_image = image.resize(new_size)
+resized_image.save("small_warrior.png")
