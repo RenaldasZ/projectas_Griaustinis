c4389d884d88d98e30ad0d48fc3fae0bc0ea3950
EricWebDev <erikasj1999@gmail.com>
2023-05-16 16:18:38 +0300

    2layout nematomas

--- giu.py
+++ giu.py
@@ -1,17 +1,24 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Image("small_warrior.png"), sg.Text("ilga istorija")],
-          [sg.Text("health"), sg.Text("power"), sg.Text("level"), sg.Button("Start")],
-          [sg.Output(s=(30, 10), key="-output-")],
-
+layout1 = [[sg.Image("small_warrior.png"), sg.Text("ilga istorija")],
+          [sg.Text("health"), sg.Text("power"), sg.Text("level"), sg.Button("Start",size=(20,3), key="-new game-")]
+        
+]
+layout2 = [[sg.Button("Left",size=(16,0), key="Left"), sg.Button("Right",size=(16,0), key="Right"), sg.Button("Foward",size=(16,0), key="Foward"),sg.Button("Back",size=(16,0), key="Back")],
+          [sg.Output(s=(30, 10), key="-output-")]
+]
+layout = [
+    [sg.Column(layout1, key='-COL1-')],
+    [sg.VSeperator()],
+    [sg.Column(layout2, key='-COL2-', visible=False)]
 ]
 
 window = sg.Window("Griaustinis", layout, size=(850, 620))
 
-
 while True:
     event, values = window.read()
-
+    if event == "-new game-":
+        window["-COL2-"].update(visible=True)
     if event == sg.WINDOW_CLOSED or event == "Close":
         break
 
