236f1645ecaa2a72c5caba1bc59f016c71b153fd
AndriusG <agedvilasplay@gmail.com>
2023-05-18 11:06:08 +0300

    update last

--- nuotraukyte.py
+++
@@ -1,9 +0,0 @@
-
-from PIL import Image
-
-
-image = Image.open("village.png")
-width, height = (564, 846)
-new_size = (150, 200) # vietoje "width" ir "height" įrašykite norimus parametrus, pvz.: (60, 80)
-resized_image = image.resize(new_size)
-resized_image.save("small_village.png")
