f6355933557d6332e83d82db76ad6ed2ab1437a6
RenaldasZ <spikeris2@gmail.com>
2023-05-18 08:31:51 +0300

    added inventory with potions

--- player_enemy_config.py
+++ player_enemy_config.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import sessionmaker
-from game_config import Player, Enemy, engine
+from game_config import Player, Enemy, InventoryItem, PowerPotion, HealthPotion, engine
 
 # Create a session
 Session = sessionmaker(bind=engine)
@@ -7,27 +7,36 @@ session = Session()
 
 # Create a player named 'Thunder Girl'
 player = Player(name='Thunder Girl', health=100, level=1, power=10, gold=10, score=0)
-session.add(player)
+
+# Add the health potion to the player's inventory
+potion = HealthPotion()
+player.add_health_potion(potion)
+
+# Add the  power potion to the player's inventory
+potion = PowerPotion()
+player.add_power_potion(potion)
+
+# Add items to the InventoryItem table
+# item1 = InventoryItem(name='Health Potion')
+# item2 = InventoryItem(name='Sword')
+# item3 = InventoryItem(name='Shield')
+
+# Add enemy to the Enemy table
+enemy1 = Enemy(name='Rat', health=50, power=10)
+enemy2 = Enemy(name='Goblin', health=110, power=12)
+enemy3 = Enemy(name='Orc', health=90, power=10)
+enemy4 = Enemy(name='Dragon', health=200, power=20)
+
+session.add_all([player, enemy1, enemy2, enemy3, enemy4])
+
+
 session.commit()
 
-# Add enemies to the Enemy table
-enemy = Enemy(name='Rat', health=50, power=10)
-session.add_all([enemy])
-session.commit()
 
-# Add enemies to the Enemy table
-enemy = Enemy(name='Goblin', health=110, power=12)
-session.add_all([enemy])
-session.commit()
+# potion = HealthPotion()
+# potion.name = "Super Health Potion"
+# session.add(potion)
+# session.commit()
 
-# Add enemies to the Enemy table
-enemy = Enemy(name='Orc', health=90, power=10)
-session.add_all([enemy])
-session.commit()
-
-# Add enemies to the Enemy table
-enemy = Enemy(name='Dragon', health=200, power=20)
-session.add_all([enemy])
-session.commit()
 
 session.close()
